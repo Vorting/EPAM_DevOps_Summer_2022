ea9826d36b3d6c0120487f3c287bca214c26f5b0
Dmytro Ivanov <ya.warkus@gmail.com>
2022-08-29 20:08:03 +0300

    create branch Task2

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+NAME=Dmytro_Ivanov
+source /home/$USER/vars
+
+echo "№1 creating dir called $NAME"
+mkdir $NAME
+
+echo "cd into $NAME"
+cd $NAME
+
+echo "№2 creating file from vars"
+#if [[ -f "/home/$USER/vars" ]]; then
+#	touch $FILE 
+#fi
+
+touch $FILE
+
+ls -la /home/$USER && ls -la /home/$USER/$NAME
